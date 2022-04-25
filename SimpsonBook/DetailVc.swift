b829a1ba109937fbd3bfe4afb54a6db48aa92286
ergeakova <ergeak@gmail.com>
2022-04-25 20:37:16 +0300

    app designed

---
+++ SimpsonBook/DetailVc.swift
@@ -0,0 +1,19 @@
+//
+//  DetailVc.swift
+//  SimpsonBook
+//
+//  Created by Erge Gevher Akova on 25.04.2022.
+//
+
+import UIKit
+
+class DetailVc: UIViewController {
+
+    @IBOutlet weak var simpsonImage: UIImageView!
+    @IBOutlet weak var simpsonName: UILabel!
+    @IBOutlet weak var simpsonJob: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
