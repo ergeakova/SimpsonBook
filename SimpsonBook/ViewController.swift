b1bd7632da78b2cd6b46220b2711d1ce35965a67
ergeakova <ergeak@gmail.com>
2022-04-25 21:19:02 +0300

    tableView settings and family image

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -7,11 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+   
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        navigationItem.title = "SimpSon Book"
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+       }
 }
