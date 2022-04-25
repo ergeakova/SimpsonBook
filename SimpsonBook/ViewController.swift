7e542de5826c4a3ee99ef220e6cf82a45ccbdb0e
ergeakova <ergeak@gmail.com>
2022-04-25 21:41:04 +0300

    add Simpson model

--- SimpsonBook/ViewController.swift
+++ SimpsonBook/ViewController.swift
@@ -8,25 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-   
+
     @IBOutlet weak var tableView: UITableView!
+    
+    var family = [Simpson]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        
+    
         navigationItem.title = "SimpSon Book"
+        
+        family.append(Simpson(nameInit: "Homer", jobInit: "Nucluear Safety" , imageInit: UIImage(named: "Homer")!))
+        family.append(Simpson(nameInit: "Bart", jobInit: "Nucluear Safety" , imageInit: UIImage(named: "Bart")!))
+        family.append(Simpson(nameInit: "Lisa", jobInit: "Nucluear Safety" , imageInit: UIImage(named: "Lisa")!))
+        family.append(Simpson(nameInit: "Maggie", jobInit: "Nucluear Safety" , imageInit: UIImage(named: "Maggie")!))
+        family.append(Simpson(nameInit: "Marge", jobInit: "Nucluear Safety" , imageInit: UIImage(named: "Marge")!))
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return family.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = family[indexPath.row].name
         return cell
        }
 }
---
+++ SimpsonBook/models/Simpson.swift
@@ -0,0 +1,22 @@
+//
+//  Simpson.swift
+//  SimpsonBook
+//
+//  Created by Erge Gevher Akova on 25.04.2022.
+//
+
+import Foundation
+import UIKit
+
+class Simpson {
+    
+    var name : String
+    var job : String
+    var image = UIImage()
+    
+    init(nameInit:String, jobInit: String, imageInit: UIImage) {
+        name = nameInit
+        job = jobInit
+        image = imageInit
+    }
+}
